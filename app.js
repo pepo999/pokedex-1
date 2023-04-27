ab2ae1a383cde338c25dd222a97507def2db3d73
labstraction <45163263+labstraction@users.noreply.github.com>
2023-04-27 10:14:38 +0200

    pageIndex

--- app.js
+++ app.js
@@ -1,27 +1,85 @@
 
+// let actualPage;
+
+// loadNextPage();
+
+// function loadNextPage(){
+//     PokeService.getNextPokemon(actualPage).then(pokemonPage => {
+//         actualPage = pokemonPage;
+//         displayPokemon();
+//         console.log(actualPage);
+//     })
+// }
+
+// function loadPreviousPage(){
+//     PokeService.getPreviousPokemon(actualPage).then(pokemonPage => {
+//         actualPage = pokemonPage;
+//         displayPokemon();
+//         console.log(actualPage);
+//     })
+// }
+
+// function displayPokemon(){
+
+//     const html = actualPage.results.map(pokemon => `<div><strong>${pokemon.name}</strong></div>`).join('');
+//     document.getElementById('pokemon-container').innerHTML = html;
+
+
+//     // //////////// map
+//     // const stringArray = []
+
+//     // for (let i = 0; i < actualPage.results.length; i++) {
+//     //     const pokemon = actualPage.results[i];
+
+//     //     const pokemonHtml =`<div><strong>${pokemon.name}</strong></div>`;
+
+//     //     stringArray.push(pokemonHtml);
+        
+//     // }
+
+//     // ///join
+//     // const allHtml = stringArray.join('')
+
+
+//     // ///innerHTML
+//     // const pokemonContainer = document.getElementById('pokemon-container');
+
+//     // pokemonContainer.innerHTML = allHtml;
+// }
+
+
+let actualPageIndex = -1;
+
 let actualPage;
 
-loadNextPage();
+loadNextPage()
 
 function loadNextPage(){
-    PokeService.getNextPokemon(actualPage).then(pokemonPage => {
+    actualPageIndex++;
+    if ( actualPageIndex >= PokeService.PAGE_COUNT){
+        actualPageIndex= 0;
+    }
+    PokeService.getPageAtIndex(actualPageIndex).then(pokemonPage => {
         actualPage = pokemonPage;
         displayPokemon();
-        console.log(actualPage);
     })
 }
 
 function loadPreviousPage(){
-    PokeService.getPreviousPokemon(actualPage).then(pokemonPage => {
+    actualPageIndex--;
+    if (actualPageIndex < 0) {
+        actualPageIndex= PokeService.PAGE_COUNT - 1;
+    }
+    PokeService.getPageAtIndex(actualPageIndex).then(pokemonPage => {
         actualPage = pokemonPage;
         displayPokemon();
-        console.log(actualPage);
     })
 }
 
 function displayPokemon(){
 
-    const html = actualPage.results
-                           .map(pokemon => `<div><strong>${pokemon.name}</strong></div>`).join('');
+    const html = actualPage.results.map(pokemon => `<div><a href="./detail?pokemon=${pokemon.name}"><strong>${pokemon.name}</strong></a></div>`).join('');
     document.getElementById('pokemon-container').innerHTML = html;
+
+
 }
--- services/poke-service.js
+++ services/poke-service.js
@@ -2,8 +2,14 @@ class PokeService{
 
     static POKE_URL = 'https://pokeapi.co/api/v2/pokemon'
 
+    static PAGE_LIMIT = 5;
+
+    static POKEMON_NUMBER = 1281;
+
+    static PAGE_COUNT = Math.ceil(this.POKEMON_NUMBER / this.PAGE_LIMIT);
+
     static getNextPokemon(page){
-        if (page === undefined) {
+        if (page === undefined || page.next === null) {
             return fetch(this.POKE_URL).then(resp => resp.json());
         } else {
             return fetch(page.next).then(resp => resp.json());
@@ -16,6 +22,21 @@ class PokeService{
         } else {
             return fetch(page.previous).then(resp => resp.json());
         }
+    }
+
+    // static getNextPage(index){
+    //     const url = this.POKE_URL + '?limit=' + this.PAGE_LIMIT + '&offset=' + (this.PAGE_LIMIT * index);
+    //     return fetch(url).then(resp => resp.json());
+    // }
+
+    // static getPreviousPage(index){
+    //     const url = this.POKE_URL + '?limit=' + this.PAGE_LIMIT + '&offset=' + (this.PAGE_LIMIT * index);
+    //     return fetch(url).then(resp => resp.json());
+    // }
+
+    static getPageAtIndex(index){
+        const url = this.POKE_URL + '?limit=' + this.PAGE_LIMIT + '&offset=' + (this.PAGE_LIMIT * index);
+        return fetch(url).then(resp => resp.json());
     }
 
 }
